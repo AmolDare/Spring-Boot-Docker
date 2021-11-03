dabfabb25da8201009409c57d6cee3d3e73de50e
AmoDare <amoldare21@gmail.com>
2021-11-03 00:09:58 -0400

    test

--- src/main/java/com/amol/springbootdocker/controllers/DockerMessageController.java
+++ src/main/java/com/amol/springbootdocker/controllers/DockerMessageController.java
@@ -2,12 +2,10 @@ package com.amol.springbootdocker.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
-public class DemoController {
-
-	@GetMapping("/")
-	public String hello() {
-		return "Hello Docker";
-	}
+public class DockerMessageController {
+	@GetMapping("/messages")
+    public String getMessage() {
+        return "Hello from Docker!";
+    }
 }
